3577fd5bc2e2be92325974259bf1a26b5bb6712f
escipion <escipion@escipion-ThinkPad-X220>
2016-02-25 00:47:23 -0300

    Carga en modal de detalle de productos

---
+++ application/controllers/Nego.php
@@ -0,0 +1,19 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+ class Neg extends MY_controller{
+     
+     public function __construct() {
+         parent::__construct();
+     }
+     
+     public function index(){
+         
+     }
+     
+     
+     
+     
+     
+     
+     
+ }
